9e236b5ecb9b18750ac5f51c06a37c8835168a81
GPramodh07 <gotivadapramodh@gmail.com>
2026-04-05 20:01:47 +0530

    back to localhost

--- backend/db.js
+++ backend/db.js
@@ -3,22 +3,18 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 4000,
-  ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'task_manager_DB',
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 pool.getConnection((err, connection) => {
-    if(err) {
+    if (err) {
         console.error("Error in connection pool:", err);
     } else {
         console.log("Database connection pool created successfully");
